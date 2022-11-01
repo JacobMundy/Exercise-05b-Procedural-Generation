72d08208a61d8ec5fed529ea04994df60e69138a
jamundy <jamundy@iu.edu>
2022-11-01 16:34:28 -0400

    Finished Assignment

--- UI/Menu.gd
+++ UI/Menu.gd
@@ -3,6 +3,8 @@ extends Control
 func _on_Restart_pressed():		# if we restart, then unpause the game and the reload the scene
 	get_tree().paused = false
 	get_tree().change_scene("res://Maze/Maze.tscn")
+	print("yo")
 
 func _on_Quit_pressed():
+	print("quit")
 	get_tree().quit()
